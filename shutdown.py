f88994487085b5df08379e08223a1ee1357376e7
Robert Unfried <robert@MacBook-Pro-von-Robert.local>
2023-01-10 17:40:25 +0100

    formatting

--- shutdown.py
+++ shutdown.py
@@ -8,17 +8,18 @@ options = {
 }
 
 
-import requests
 def read_from_file():
     with open('./servers.txt') as f:
         for line in f:
             yield line
+
 
 def shutdown_servers():
     for server in read_from_file():
         print(url % server.strip())
         #requests.post((url % server), json=options)
         pass
+
 
 if __name__ == '__main__':
     shutdown_servers()
