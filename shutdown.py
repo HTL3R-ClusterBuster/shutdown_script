3f4a02776adcca086a41dd2a0627dc3a641e9caa
Robert Unfried <robert@MacBook-Pro-von-Robert.local>
2023-01-10 17:38:55 +0100

    added API call

--- shutdown.py
+++ shutdown.py
@@ -2,3 +2,23 @@
 # IDRAC IP addresses are listed in the
 # file servers.txt
 
+url = 'https://%s/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset'
+options = {
+    'ResetType': 'PushPowerButton'
+}
+
+
+import requests
+def read_from_file():
+    with open('./servers.txt') as f:
+        for line in f:
+            yield line
+
+def shutdown_servers():
+    for server in read_from_file():
+        print(url % server.strip())
+        #requests.post((url % server), json=options)
+        pass
+
+if __name__ == '__main__':
+    shutdown_servers()
