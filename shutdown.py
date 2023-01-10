aa7de6d9656701448d803e45b541567242c6701b
Robert Unfried <robert@MacBook-Pro-von-Robert.local>
2023-01-10 17:42:32 +0100

    Added documentation

--- shutdown.py
+++ shutdown.py
@@ -2,19 +2,30 @@
 # IDRAC IP addresses are listed in the
 # file servers.txt
 
+
+# url to API shutdown route of the IDRAC Redfish API
 url = 'https://%s/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset'
+
+# power option for request json data
 options = {
     'ResetType': 'PushPowerButton'
 }
 
 
 def read_from_file():
+    """
+    read all servers from file servers.txt
+    :return: all IP addr in file
+    """
     with open('./servers.txt') as f:
         for line in f:
             yield line
 
 
 def shutdown_servers():
+    """
+    send the shutdown request for each server
+    """
     for server in read_from_file():
         print(url % server.strip())
         #requests.post((url % server), json=options)
@@ -22,4 +33,5 @@ def shutdown_servers():
 
 
 if __name__ == '__main__':
+    # call shutdown_server() method
     shutdown_servers()
