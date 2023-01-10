a6bb50857eeec17cddc2264b2cf7adc698016620
Robert Unfried <robert@MacBook-Pro-von-Robert.local>
2023-01-10 21:42:04 +0100

    Added .env check

--- shutdown.py
+++ shutdown.py
@@ -1,7 +1,11 @@
 # Script shutdowns the servers which
 # IDRAC IP addresses are listed in the
 # file servers.txt
+import sys
+
 import requests
+import os
+from dotenv import load_dotenv
 
 # url to API shutdown route of the IDRAC Redfish API
 url = 'https://%s/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset'
@@ -28,10 +32,25 @@ def shutdown_servers():
     """
     for server in read_from_file():
         print(url % server.strip())
-        requests.post((url % server), json=options)
+        #requests.post((url % server), json=options)
         pass
 
+def check_env():
+    """
+    checks if .env variables are configured
+    :return dictionary with the env Variables
+    """
+    required = ['USER', 'PASSWORD']
+    load_dotenv()
+    for var in required:
+        if not os.getenv(var):
+            print(f'ERROR in .env! {var} is required but not configured.')
+            sys.exit()
+    return vars
 
 if __name__ == '__main__':
+    # check if env is configured correctly
+    check_env()
+
     # call shutdown_server() method
     shutdown_servers()
